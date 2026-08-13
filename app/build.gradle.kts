69abab5dfedc0c15a7da20d6e8eb0e354274dabe
Lutfor Rahman <lutforurc@gmail.com>
2026-08-13 12:21:00 +0600

    Tidy the tenant URL list's commented lines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,9 @@ val hasReleaseSigning = keystoreProps.getProperty("storeFile")
 // val baseUrl = "https://krf.cashbookbd.com/api/" 
 // val baseUrl = "https://scn.cashbookbd.com/api/" 
 // val baseUrl = "https://mbdpp.cashbookbd.com/api/" 
-// val baseUrl = "https://kps.cashbookbd.com/api/" 
-// val baseUrl = "https://kbr.cashbookbd.com/api/" 
-val baseUrl = "https://my.cashbookbd.com/api/"
+//val baseUrl = "https://kps.cashbookbd.com/api/"
+// val baseUrl = "https://kbr.cashbookbd.com/api/"
+ val baseUrl = "https://my.cashbookbd.com/api/"
 
 
 
